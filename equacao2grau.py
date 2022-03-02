411e9dd9fec025011f8009ad38f9954abe2a6544
rafaeltomachado <97458722+rafaeltomachado@users.noreply.github.com>
2022-03-01 21:52:58 -0300

    Update equacao2grau.py

--- equacao2grau.py
+++ equacao2grau.py
@@ -9,14 +9,17 @@ b = float(input("\nDigite o valor de b:\n"));
 c = float(input("\nDigite o valor de c:\n"));
 
 delta = float(pow(b,2) - 4*a*c);
-x1 = float((0 - b + (pow(delta, 1/2)))/2*a);
-x2 = float((0 - b - (pow(delta, 1/2)))/2*a);
+
+if (delta > 0) or (delta == 0):
+    x1 = float((0 - b + (pow(delta, 1/2)))/2*a);
+    x2 = float((0 - b - (pow(delta, 1/2)))/2*a);
 
 if delta < 0:
     x1 = 0;
     x2 = 0;
     print("\n");
     print("Esta equação poderá ser resolvida por números complexos");
+
 if delta > 0:
     print("\n");
     print("As raízes desta equação são", x1, "e", x2);
